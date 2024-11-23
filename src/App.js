02a62a3b2191796b7c5a56ccf57c53dc0340d8f8
Favour Ikechukwu <149949474+Favour-Ikec@users.noreply.github.com>
2024-11-23 13:28:23 +0100

    New Stuff
    Just the Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
